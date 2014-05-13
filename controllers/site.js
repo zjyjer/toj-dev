fecd63e22b5782edc263db7b20897e8a67f2ac01
jie414341055 <jie414341055@gmail.com>
2014-05-13 22:28:21 +0800

    首页正在running比赛和scheduled比赛可以正常显示了

--- controllers/site.js
+++ controllers/site.js
@@ -2,6 +2,7 @@
  * Module dependecies
  */
 
+var crypto = require('crypto');
 var EventProxy = require('eventproxy');
 var config = require('../config').config;
 var Util = require('../libs/util');
@@ -9,13 +10,18 @@ var Contest = require('../proxy').Contest;
 var Recent_Contest = require('../proxy').Recent_Contest;
 var OJ_Status = require('../proxy').OJ_Status;
 
+
 exports.index = function(req, res, next) {
 
-	var events = ['rconts', 'oj_status'];
-	var ep = EventProxy.create(events, function(rconts, oj_status) {
+	var events = ['rconts', 'oj_status', 'run_conts0', 'run_conts1', 'sch_conts0', 'sch_conts1'];
+	var ep = EventProxy.create(events, function(rconts, oj_status, run_conts0, run_conts1, sch_conts0, sch_conts1) {
 		res.render('index', {
 			title: 'Home',
 			fcontests: rconts,
+			frun_conts0: run_conts0,
+			frun_conts1: run_conts1,
+			fsch_conts0: sch_conts0,
+			fsch_conts1: sch_conts1,
 			foj_status: oj_status,
 			foj_msg: config.oj_msg,
 		});
@@ -30,6 +36,19 @@ exports.index = function(req, res, next) {
 
 	OJ_Status.getAllStatus(ep.done('oj_status'));
 
+	now = new Date();
+	var query = { start_time: { $lt: now }, end_time: { $gt: now }, type: 0 };
+	var options = { limit: 5 };
+	Contest.getMulti(query, {}, options, ep.done('run_conts0'));
+
+	query = { start_time: { $lt: now }, end_time: { $gt: now }, type: 1 };
+	Contest.getMulti(query, {}, options, ep.done('run_conts1'));
+
+	query = { start_time: { $gt: now }, type: 0 };
+	Contest.getMulti(query, {}, options, ep.done('sch_conts0'));
+
+	query = { start_time: { $gt: now }, type: 1 };
+	Contest.getMulti(query, {}, options, ep.done('sch_conts1'));
 };
 
 
